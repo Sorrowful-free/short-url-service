22679de8e785d5d38c5f6f18ec62d91230a1581c
sorrowful.free <sorrowful.free@gmail.com>
2025-08-17 18:47:55 +0300

    - fix base url in handlers

--- internal/handler/handler_init.go
+++ internal/handler/handler_init.go
@@ -4,10 +4,10 @@ import "github.com/Sorrowful-free/short-url-service/internal/service"
 
 var (
 	internalURLService service.ShortURLService
-	baseURL            string
+	internalBaseURL    string
 )
 
 func Init(urlService service.ShortURLService, baseURL string) {
 	internalURLService = urlService
-	baseURL = baseURL
+	internalBaseURL = baseURL
 }
