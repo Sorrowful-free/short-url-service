2bf5c8e8926df1945051e001489f970295a70c75
Sergei Kotkin <sorrowful.free@gmail.com>
2025-09-15 20:13:45 +0300

    -test config cleanup

--- internal/config/local_config.go
+++ internal/config/local_config.go
@@ -30,9 +30,9 @@ func GetLocalConfig() *LocalConfig {
 	flag.StringVar(&localConfig.ListenAddr, "a", "localhost:8080", "listen address")
 	flag.StringVar(&localConfig.BaseURL, "b", "http://localhost:8080", "base URL")
 	flag.IntVar(&localConfig.UIDLength, "l", 8, "length of the short URL")
-	flag.StringVar(&localConfig.FileStoragePath, "f", "tmp_short_urls.json", "file storage path")
-	flag.StringVar(&localConfig.MigrationsPath, "m", "file://./migrations", "migrations path")
-	flag.StringVar(&localConfig.DatabaseDSN, "d", "postgresql://postgres:postgres@localhost:5432/short_urls?sslmode=disable", "postgres DSN")
+	flag.StringVar(&localConfig.FileStoragePath, "f", "", "file storage path")
+	flag.StringVar(&localConfig.MigrationsPath, "m", "file://./../../migrations", "migrations path")
+	flag.StringVar(&localConfig.DatabaseDSN, "d", "", "postgres DSN")
 	flag.Parse()
 
 	//override default values with values from environment variables if they are set
