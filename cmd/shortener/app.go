4148c21b1f69ed6ce8f228cd933a184a5c3b37f6
Sergei Kotkin <sorrowful.free@gmail.com>
2025-09-15 10:27:39 +0300

    - make something like composition root

---
+++ cmd/shortener/app.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/Sorrowful-free/short-url-service/internal/config"
+	"github.com/Sorrowful-free/short-url-service/internal/handler"
+	"github.com/Sorrowful-free/short-url-service/internal/logger"
+	"github.com/Sorrowful-free/short-url-service/internal/middlewares"
+	"github.com/Sorrowful-free/short-url-service/internal/service"
+	"github.com/labstack/echo/v4"
+)
+
+type App struct {
+	internalConfig     *config.LocalConfig
+	internalLogger     logger.Logger
+	internalEcho       *echo.Echo
+	internalURLService service.ShortURLService
+	internalDBService  service.DBService
+}
+
+func NewApp() *App {
+	return &App{}
+}
+
+func (a *App) InitLogger() error {
+	l, err := logger.NewZapLogger()
+	if err != nil {
+		return err
+	}
+	a.internalLogger = l
+	return nil
+}
+
+func (a *App) InitConfig() error {
+	a.internalConfig = config.GetLocalConfig()
+	return nil
+}
+
+func (a *App) InitURLService() error {
+	urlService, err := service.NewSimpleService(a.internalConfig.UIDLength, a.internalConfig.FileStoragePath, a.internalLogger)
+	if err != nil {
+		return err
+	}
+	a.internalURLService = urlService
+	return nil
+}
+
+func (a *App) InitDBService() error {
+	dbService, err := service.NewPostgresDBService(a.internalConfig.DatabaseDSN)
+	if err != nil {
+		return err
+	}
+	a.internalDBService = dbService
+	return nil
+}
+
+func (a *App) InitHandlers() error {
+	e := echo.New()
+	e.Use(middlewares.LoggerAsMiddleware(a.internalLogger))
+	e.Use(middlewares.GzipMiddleware(a.internalLogger))
+	handlers := handler.NewHandlers(e, a.internalURLService, a.internalDBService, a.internalConfig.BaseURL)
+	handlers.RegisterHandlers()
+	a.internalEcho = e
+	return nil
+}
+
+func (a *App) Init() error {
+	if err := a.InitLogger(); err != nil {
+		return err
+	}
+	if err := a.InitConfig(); err != nil {
+		return err
+	}
+	if err := a.InitURLService(); err != nil {
+		return err
+	}
+	if err := a.InitDBService(); err != nil {
+		return err
+	}
+	if err := a.InitHandlers(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *App) Run() error {
+	return a.internalEcho.Start(a.internalConfig.ListenAddr)
+}
--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,42 +2,15 @@ package main
 
 import (
 	"log"
-
-	"github.com/Sorrowful-free/short-url-service/internal/config"
-	"github.com/Sorrowful-free/short-url-service/internal/handler"
-	"github.com/Sorrowful-free/short-url-service/internal/logger"
-	"github.com/Sorrowful-free/short-url-service/internal/middlewares"
-	"github.com/Sorrowful-free/short-url-service/internal/service"
-	"github.com/labstack/echo/v4"
 )
 
 func main() {
 
-	err := run()
-	if err != nil {
+	app := NewApp()
+	if err := app.Init(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func run() error {
-	lc := config.GetLocalConfig()
-
-	e := echo.New()
-	l, err := logger.NewZapLogger()
-	if err != nil {
-		return err
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
 	}
-	e.Use(middlewares.LoggerAsMiddleware(l))
-	e.Use(middlewares.GzipMiddleware(l))
-	s, err := service.NewSimpleService(lc.UIDLength, lc.FileStoragePath, l)
-	if err != nil {
-		return err
-	}
-
-	dbService, err := service.NewPostgresDBService(lc.DatabaseDSN)
-	if err != nil {
-		return err
-	}
-	handler.NewHandlers(e, s, dbService, lc.BaseURL).RegisterHandlers()
-	return e.Start(lc.ListenAddr)
 }
